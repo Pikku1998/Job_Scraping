8b34ce66631111001e3df40b24742eaf7a875ea9
Prakash R <prakashramanukolanu1998@gmail.com>
2023-08-21 11:55:36 +0530

    Extract required data(recent job posting) from page source.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import selectorlib
+
 
 URL = 'https://www.linkedin.com/jobs/search?keywords=Python%20Django%20Developer&location=India&locationId=&geoId' \
       '=102713980&f_TPR=r604800&f_PP=105214831%2C105556991%2C106888327%2C103671728&f_E=1%2C2%2C3&position=1&pageNum=0'
@@ -10,5 +12,12 @@ def get_source(url):
     return source
 
 
+def extract_data(page_source):
+    extractor = selectorlib.Extractor.from_yaml_file('jobs.yaml')
+    data = extractor.extract(page_source)
+    return data
+
+
 if __name__ == '__main__':
-    print(get_source(URL))
+    source = get_source(URL)
+    print(extract_data(source))
